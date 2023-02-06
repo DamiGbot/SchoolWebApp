2e067ad6cec234da426f8fcefcffa63d029c042d
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2023-02-06 14:08:50 +0000

    Update JPA

--- src/main/java/com/ravenschool/web_example_1/EazySchoolApplication.java
+++ src/main/java/com/ravenschool/web_example_1/EazySchoolApplication.java
@@ -2,8 +2,12 @@ package com.ravenschool.web_example_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.ravenschool.web_example_1.Repository")
+@EntityScan("com.ravenschool.web_example_1.Model")
 public class EazySchoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ravenschool/web_example_1/Repository/IContactRepository.java
+++ src/main/java/com/ravenschool/web_example_1/Repository/IContactRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface IContactRepository extends CrudRepository<Contact, Integer> {
 
-    // Derived Queries
+    // Derived Queries abstract methods
     List<Contact> findByStatus(String status);
 }
